8b237d136602f21787affe472e4baaf89d4343ef
Abhinav Kumar <dynamic2106@gmail.com>
2023-12-26 19:48:15 +0530

    #Added Authentication in our API #Configured program.cs and authentication MW to validate our JWT

---
+++ WEB API/Controllers/AuthController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace WEB_API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public AuthController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [HttpPost]
+        public IActionResult Authenticate([FromBody] Credential credential)
+        {
+            // verfiy the cred
+            if (credential.UserName == "admin" && credential.Password == "password")
+            {
+                // creating the security Context for JWT
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,"Admin"),
+                    new Claim(ClaimTypes.Email,"admin@mywebsite.com"),
+                    new Claim("Department","HR"), // Added a new Department claim to Authorize the HR page policy
+                                                 // because HR page policy require a claim Department with HR value
+                    new Claim("Admin","true"), // Added a new claim Admin to resolve AdminOnly Policy 
+                    new Claim("Manager","true"), // Added a new claim to resolve ManagerClaim Policy
+
+                    new Claim("EmploymentDate","2021-02-01") // Added a new Claim for complex Authorization 
+                };
+
+                var expiryAt = DateTime.UtcNow.AddMinutes(10); //Added expiry time
+
+                return Ok(new
+                {
+                    access_token = CreateToken(claims,expiryAt),  // it is empty here now. We will create a pvt method to get the token
+                    expires_at = expiryAt // It is for client 
+                });
+            }
+            ModelState.AddModelError("Unauthorized", "You are not authorized to access the endpoint");
+            return Unauthorized(ModelState);
+        }
+
+        private string CreateToken(IEnumerable<Claim> claims, DateTime expireAt) // method to generate access token
+        {
+            var secretKey = Encoding.ASCII.GetBytes(_config.GetValue<string>("secretKey")??""); // getting secretKey in Bytes
+            // generate JWT
+            // install nuget package
+            var jwt = new JwtSecurityToken(   // creating JWT object
+                claims: claims,
+                notBefore: DateTime.UtcNow,
+                expires: expireAt,
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(secretKey), SecurityAlgorithms.HmacSha256Signature));
+
+            return new JwtSecurityTokenHandler().WriteToken(jwt); // convert jwt object to string and return
+        }
+         
+        public class Credential
+        {
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }
+    }
+}
--- WEB API/Program.cs
+++ WEB API/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,27 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//configureing our Authentication MW for JWT validation
+var secretKey = builder.Configuration.GetValue<string>("secretKey");
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey ?? string.Empty)),
+        ValidateLifetime = true,
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        ClockSkew = TimeSpan.Zero,
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,7 +42,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication(); // Added Authnetication MW
 app.UseAuthorization();
 
 app.MapControllers();
